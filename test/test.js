9ce82196a7c5d06fcd2eece75bd34fc9a41468f4
doumKim <eha0129@naver.com>
2020-06-27 18:04:03 +0900

    UPDATE: Changed test.js to use chai

--- test/test.js
+++ test/test.js
@@ -1,14 +1,8 @@
 // let findUserAge = require('../findUserAge');
+import chai from 'chai';
 import { findUserAge } from '../index';
 
-let assert = require('assert');
-describe('Array', function () {
-	describe('#indexOf()', function () {
-		it('should return -1 when the value is not present', function () {
-			assert.equal([1, 2, 3].indexOf(4), -1);
-		});
-	});
-});
+let assert = chai.assert;
 
 describe('findUserAge', function () {
 	let users = [
